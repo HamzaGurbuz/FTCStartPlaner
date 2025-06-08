de0e28a980acd05e842642205fe7b45bb33cf788
Necmettin Hamza Gürbüz <necmettinhamzagurbuzz@gmail.com>
2025-06-08 21:12:18 +0300

    Add Example Setting

--- app/src/main/java/com/example/ftcstartplaner/SettingsActivity.java
+++ app/src/main/java/com/example/ftcstartplaner/SettingsActivity.java
@@ -1,4 +1,14 @@
 package com.example.ftcstartplaner;
 
-public class SettingsActivity {
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SettingsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        // Buraya ayarlarla ilgili kodları ekleyebilirsin.
+    }
 }
